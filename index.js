2f79640eaf4fc14410832ac35c9f6353b6b61ad0
jshim00 <shimjustin2000@gmail.com>
2024-07-06 23:41:27 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function scrollToResume() {
+    document.getElementById("resume").scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToProjects() {
+    document.getElementById("projects").scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToFooter() {
+    document.getElementById("footer").scrollIntoView({ behavior: 'smooth' });
+}
